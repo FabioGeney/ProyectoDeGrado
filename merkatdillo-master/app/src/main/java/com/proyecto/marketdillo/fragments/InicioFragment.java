16a1a5b8aa10e8987b9d7bdc70f4e46f9ed82087
Sebastián Giraldo <sebastiao_093@hotmail.com>
2019-04-29 17:16:57 -0500

    Puntos
    Se agregó los puntos

--- merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/fragments/InicioFragment.java
+++ merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/fragments/InicioFragment.java
@@ -4,10 +4,12 @@ package com.proyecto.marketdillo.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.proyecto.marketdillo.R;
 import com.proyecto.marketdillo.SliderAdapter;
@@ -19,6 +21,7 @@ public class InicioFragment extends Fragment{
 
     private ViewPager slideViewPager;
     private LinearLayout dotsLayout;
+    private TextView[] dots;
 
     private SliderAdapter sliderAdapter;
 
@@ -38,8 +41,49 @@ public class InicioFragment extends Fragment{
 
         sliderAdapter = new SliderAdapter(getActivity());
         slideViewPager.setAdapter(sliderAdapter);
+        addDotsIndicator(0);
+        slideViewPager.addOnPageChangeListener(viewListener);
 
         return root;
     }
+
+    public void addDotsIndicator(int position){
+
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+
+        for(int i = 0; i < dots.length; i++){
+
+            dots[i] = new TextView(getActivity());
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+            dotsLayout.addView(dots[i]);
+
+        }
+
+        if(dots.length > 0){
+
+            dots[position].setTextColor(getResources().getColor(R.color.colorPrimary2));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
 
 }
