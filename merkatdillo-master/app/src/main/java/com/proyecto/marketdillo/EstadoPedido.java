0fb9b7911a40a721564a43bcd5064e7e8a016b12
FabioGeney <37422482+FabioGeney@users.noreply.github.com>
2019-06-07 11:52:11 -0500

    EstdoPedidoConsumidor
    Se implemento el activity para mostrar el progreso del pedido en las vistas del consumidor

---
+++ merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/EstadoPedido.java
@@ -0,0 +1,13 @@
+package com.proyecto.marketdillo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EstadoPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estado_pedido);
+    }
+}
--- merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/FirebaseMessagingService.java
+++ merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/FirebaseMessagingService.java
@@ -1,6 +1,9 @@
 package com.proyecto.marketdillo;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
@@ -20,9 +23,14 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
                 .setContentText(cuerpo)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        Intent intent = new Intent();
+        PendingIntent intentFilter = PendingIntent.getActivity(  this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(intentFilter);
+
         int mNotificationID = (int)System.currentTimeMillis();
         NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(mNotificationID, builder.build());
+
 
 
     }
