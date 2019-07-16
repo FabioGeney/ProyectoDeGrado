34c33d0e05afa5c79d4f9bba3950728997951f9f
Sebastián Giraldo <sebastiao_093@hotmail.com>
2019-07-15 21:52:21 -0500

    Trabajando Phone
    Parte del Phone Activity

---
+++ merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/CrearPhoneCampesinoActivity.java
@@ -0,0 +1,13 @@
+package com.proyecto.marketdillo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearPhoneCampesinoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_phone_campesino);
+    }
+}
