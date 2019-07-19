300086940ca635687912c0e2c123db6ee7b472d4
Sebastián Giraldo <sebastiao_093@hotmail.com>
2019-07-19 02:14:09 -0500

    Formularios telefono
    Creando formularios y vista

--- merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/CrearPhoneActivity.java
+++ merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/CrearPhoneActivity.java
@@ -2,12 +2,40 @@ package com.proyecto.marketdillo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CrearPhoneActivity extends AppCompatActivity {
+
+    private static final String TAG = "CrearPhoneActivity";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private EditText etnombres;
+    private EditText etapellidos;
+    private String etemail = "no tiene";
+    private EditText etcelular;
+    private EditText etfechanacimiento;
+    private EditText etdidentidad;
+    private EditText etdireccion;
+    private Button bnsiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_phone);
+
+        etnombres =findViewById(R.id.etnombres);
+        etapellidos = findViewById(R.id.etapellidos);
+        etcelular = findViewById(R.id.etcelular);
+        etfechanacimiento = findViewById(R.id.etfechanacimiento);
+        etdidentidad = findViewById(R.id.etdidentidad);
+        etdireccion = findViewById(R.id.etdireccion);
+        bnsiguiente = findViewById(R.id.bnsiguiente);
+
     }
 }
--- merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/CrearPhoneCampesinoActivity.java
+++ merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/CrearPhoneCampesinoActivity.java
@@ -2,12 +2,45 @@ package com.proyecto.marketdillo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CrearPhoneCampesinoActivity extends AppCompatActivity {
+
+    private static final String TAG = "CrearPhoneCampesinoActivity";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private EditText etnombres;
+    private EditText etapellidos;
+    private String etemail = "no tiene";
+    private EditText etcelular;
+    private EditText etfechanacimiento;
+    private EditText etdidentidad;
+    private EditText etdireccion;
+    private EditText etmercadillo;
+    private EditText ettiempoaprox;
+    private Button bnsiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_phone_campesino);
+
+        etnombres =findViewById(R.id.etnombres);
+        etapellidos = findViewById(R.id.etapellidos);
+        etcelular = findViewById(R.id.etcelular);
+        etfechanacimiento = findViewById(R.id.etfechanacimiento);
+        etdidentidad = findViewById(R.id.etdidentidad);
+        etdireccion = findViewById(R.id.etdireccion);
+        etmercadillo = findViewById(R.id.etmercadillo);
+        ettiempoaprox = findViewById(R.id.ettiempoaprox);
+        bnsiguiente = findViewById(R.id.bnsiguiente);
+
+
     }
 }
