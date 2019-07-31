5786869593b6ad5c330429646262cbc882b4f0bd
Sebastián Giraldo <sebastiao_093@hotmail.com>
2019-07-31 04:05:03 -0500

    Splash screen
    Se implemento el splash screen

---
+++ merkatdillo-master/app/src/main/java/com/proyecto/marketdillo/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.proyecto.marketdillo;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, BottomActivity.class);
+                startActivity(i);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
